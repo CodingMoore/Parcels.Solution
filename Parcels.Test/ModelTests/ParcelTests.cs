2ee2b3e07f0bb6a43b9e9d40d3829524ddf7fcb2
Randel M <Randel.C.Moore@gmail.com>
2020-12-15 13:41:14 -0800

    add test to check that properties are applied to Parcels
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

--- Parcels.Test/ModelTests/ParcelTests.cs
+++ Parcels.Test/ModelTests/ParcelTests.cs
@@ -20,5 +20,17 @@ namespace Parcels.Test
       Parcel box = new Parcel(5, 6, 7, 10);
       Assert.AreEqual(typeof(Parcel), box.GetType());
     }
+
+    [TestMethod]
+
+    public void ParcelConstructor_ReturnProperties_True()
+    {
+      Parcel box = new Parcel(5, 6, 7, 10);
+      Assert.AreEqual(5, box.Length);
+      Assert.AreEqual(6, box.Width);
+      Assert.AreEqual(7, box.Height);
+      Assert.AreEqual(11, box.Weight);
+
+    } 
   }
 }
